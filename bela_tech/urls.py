2d43f37a4a502099d550a99fa00498e1bfc3cce8
jayed969 <jayed.swe@gmail.com>
2022-11-17 23:51:20 +0600

    aded static files & configuration

--- bela_tech/urls.py
+++ bela_tech/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('home.urls'))
+    path('', include('home.urls'))
 ]
 
 if settings.DEBUG:
