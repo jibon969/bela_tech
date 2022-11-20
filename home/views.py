e273812171b002730f9678b586e89058316e7c39
jayed969 <jayed.swe@gmail.com>
2022-11-20 10:30:05 +0600

    added some logo & create contact models

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+import csv
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib import messages
+from .forms import ContactForm, ReplayForm
+from .models import Contact
+from django.conf import settings
+from django.db.models import Q
+from django.core.mail import send_mail
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def home(request):
@@ -9,4 +16,19 @@ def home(request):
     :param request:
     :return:
     """
-    return render(request, "home/home.html")
+    form = ContactForm(request.POST or None)
+    errors = None
+    if form.is_valid():
+        form.save()
+        messages.add_message(request, messages.SUCCESS, "Success! Thank you for your message.")
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    if form.errors:
+        errors = form.errors
+    context = {
+        'form': form,
+        'errors': errors
+    }
+
+    return render(request, "home/home.html", context)
+
+
